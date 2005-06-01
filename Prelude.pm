d829ac2f3e6cb72456e3a43e0aa9cf03bc88cc90
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-01 00:55:30 +0000

    add eval and evalfile to Prelude (need Prim support) mark subs unsafe (need core support)
    git-svn-id: http://svn.pugscode.org/pugs@4235 c213334d-75ef-0310-aa23-eaa082d1ae64

--- Prelude.pm
+++ Prelude.pm
@@ -21,7 +21,7 @@ class File;
 # spec, see the thread rooted at <20050502192508.GF24107@sike.forum2.org>
 # on p6-l.
 
-multi sub open (Str $filename, Str +$layer, Bool +$r, Bool +$w, Bool +$rw, Bool +$a) returns IO is primitive {
+multi sub open (Str $filename, Str +$layer, Bool +$r, Bool +$w, Bool +$rw, Bool +$a) returns IO is primitive is unsafe {
     die "fancy open modes not supported yet" if $a & any($r, $w, $rw);
     my $mode;
     $mode = "a" if $a;
@@ -44,7 +44,7 @@ class Pipe;
 
 # Easy to use, unidirectional pipe. Uses the shell.
 
-multi sub open (Str $command, Bool +$r is copy, Bool +$w) returns IO is primitive {
+multi sub open (Str $command, Bool +$r is copy, Bool +$w) returns IO is primitive is unsafe {
     die "Pipe::open is unidirectional" if all($r, $w);
     $r = bool::true if none($r, $w);
     my ($in, $out, $err, undef) =
@@ -56,7 +56,7 @@ multi sub open (Str $command, Bool +$r is copy, Bool +$w) returns IO is primitiv
 
 # Bidirectional pipe. Potenially dangerous. Uses the shell.
 
-multi sub open2 (Str $command) returns List is primitive {
+multi sub open2 (Str $command) returns List is primitive is unsafe {
     my ($in, $out, $err, $pid) =
         Pugs::Internals::runInteractiveCommand($command);
     close $err;
@@ -65,8 +65,42 @@ multi sub open2 (Str $command) returns List is primitive {
 
 # Tridirectional pipe. Potenially dangerous. Uses the shell.
 
-multi sub open3 (Str $command) returns List is primitive {
+multi sub open3 (Str $command) returns List is primitive is unsafe {
     my ($in, $out, $err, $pid) =
         Pugs::Internals::runInteractiveCommand($command);
     ($in, $out, $err, $pid);
+}
+
+
+class Control::Basic;
+
+# multi-lingual eval.
+
+# S29:
+# Execute C<$code> as if it were code written in C<$lang>. C<Perl6> is the
+# only required language, but supporting C<Perl5> is I<strongly>
+# recommended.
+
+# XXX: don't call me yet, the Prim.hs side of this isn't done.
+# migrating all of eval takes updating lots of code.
+
+# XXX: mark everything except :lang<YAML> unsafe
+# (maybe :lang<YAML> doesn't quite belong here?)
+multi sub eval (: Str $code = $CALLER::_, Str +$lang = 'Perl6')
+        is primitive is unsafe {
+    given $lang {
+        when 'Perl6'   { Pugs::Internals::eval($code) }
+        when 'Perl5'   { Pugs::Internals::eval_perl5($code) }
+        when 'Haskell' { Pugs::Internals::eval_haskell($code) }
+        when 'Parrot'  { Pugs::Internals::eval_parrot($code) }
+        when 'YAML'    { Pugs::Internals::eval_yaml($code) }
+    }
+}
+
+# S29:
+# Behaves like, and replaces Perl 5 C<do EXPR>, with optional C<$lang>
+# support.
+multi sub evalfile (Str $filename : Str +$lang = 'Perl6')
+        is primitive is unsafe {
+    eval(slurp $filename, $lang);
 }
