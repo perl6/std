7829cab16d8c12d0adf57f9f24391790e4cec87a
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-19 14:44:12 +0000

    Prelude::PIR, Emit.PIR, Compile.PIR -- Implemented &sleep and made &exit call all END blocks.
    git-svn-id: http://svn.pugscode.org/pugs@4846 c213334d-75ef-0310-aa23-eaa082d1ae64

--- Prelude/PIR.pm
+++ Prelude/PIR.pm
@@ -2,9 +2,9 @@
 # XXX -- for some reason, compilation doesn't work if the above line is uncommented.
 
 # our &prefix:<?> := &true doesn't work yet.
-sub prefix:<?> ($var) { true $var }
+sub prefix:<?> ($var) returns Bool { true $var }
 
-sub chomp (Str $str is rw) {
+sub chomp (Str $str is rw) returns Str {
     if substr($str, -1, 1) eq "\n" {
         $str = substr $str, 0, chars($str) - 1;
         "\n";
@@ -13,7 +13,7 @@ sub chomp (Str $str is rw) {
     }
 }
 
-sub chop (Str $str is rw) {
+sub chop (Str $str is rw) returns Str {
     if chars($str) == 0 {
         undef;
     } else {
@@ -21,4 +21,15 @@ sub chop (Str $str is rw) {
         $str = substr $str, 0, chars($str) - 1;
         $removed;
     }
+}
+
+sub sleep (Num $seconds) returns Num {
+    my $time = time;
+    Pugs::Internals::sleep $seconds;
+    my $seconds_slept = time() - $time;
+    $seconds_slept;
+}
+
+sub exit (Int ?$status = 0) {
+    Pugs::Internals::exit $status;
 }
