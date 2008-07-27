d444ec7a815237b9c625b4dac8f35056d67c4ab3
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-07-27 23:23:41 +0000

    [STD] Added the 'let' and 'temp' scope_declarator's.
    git-svn-id: http://svn.pugscode.org/pugs@21585 c213334d-75ef-0310-aa23-eaa082d1ae64

--- STD.pm
+++ STD.pm
@@ -1063,6 +1063,8 @@ rule scoped {
 
 token scope_declarator:my       { <sym> <scoped> }
 token scope_declarator:our      { <sym> <scoped> }
+token scope_declarator:let      { <sym> <scoped> }
+token scope_declarator:temp     { <sym> <scoped> }
 token scope_declarator:state    { <sym> <scoped> }
 token scope_declarator:constant { <sym> <scoped> }
 token scope_declarator:has      { <sym> <scoped> }
