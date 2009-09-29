b37ee4fcec763d649892c792e1392ca0b4aaf8f1
mberends <mberends@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-09-29 17:26:29 +0000

    [sprixel.mk] updated 'test' and 'spectest' targets
    git-svn-id: http://svn.pugscode.org/pugs@28498 c213334d-75ef-0310-aa23-eaa082d1ae64

--- sprixel.mk
+++ sprixel.mk
@@ -1,14 +1,20 @@
 # Makefile for sprixel
 
-# 
+# list of simple tests that sprixel passes
 T_01_SANITY = ../../t/01-sanity/04-if.t
+TESTS = $(T_01_SANITY)
+
+#  list of spectests that sprixel passes
+T_SPEC_S02_BUILTIN_DATA_TYPES = ../../t/spec/S02-builtin_data_types/num.t
+SPECTESTS = $(T_SPEC_S02_BUILTIN_DATA_TYPES)
 
 # usage: make -f sprixel.mk test
 test:
-	prove -e 'perl sprixel.pl' $(T_01_SANITY)
+	prove -e 'perl sprixel.pl' $(TESTS)
 	@echo Q.E.D.
 
+# usage: make -f sprixel.mk spectest
 spectest:
-	perl ~/rakudo/t/harness prove -e 'perl vijs' vivjs/t/
+	prove -e 'perl sprixel.pl' $(SPECTESTS)
 	@echo Q.E.D.
 
