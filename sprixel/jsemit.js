2cf87c656d21efc2aad667c537b2308673402043
diakopter <diakopter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-01-17 15:40:08 +0000

    [sprixel] implemented some of the default pattern methods. implemented a ranges() emitter (naively supports 0000-FFFF only; will have to suffice until libicu is integrated, which will provide quick generation from the real Unicode 5.2ff tables. implemented xor() combinator, a deterministic (non-backtracking) alternation among (for now) deterministic choice patterns.  useful for composing exclusive match checks, such as multiple codepoint ranges (so backtracking information is not stored).
    git-svn-id: http://svn.pugscode.org/pugs@29551 c213334d-75ef-0310-aa23-eaa082d1ae64

--- sprixel/jsemit.js
+++ sprixel/jsemit.js
@@ -109,7 +109,7 @@ var derives = (function() {
 
 var derive = (function() {
   function F() {} // derive an object whose prototype chain inherits from another
-  return function(obj, parent) {
+  return function(parent) {
     F.prototype = parent;
     return new F();
   };
@@ -362,7 +362,9 @@ function gfail() { // grammar '<!> (never match anything)' parser builder
 }
 derives(gfail, gts);
 gfail.prototype.emit = function(c) {
-  gotol(this.fail)
+  c.r.push(
+    gotol(this.fail)
+  );
 };
 gfail.prototype.toString = function() {
   return 'fail()';
@@ -383,7 +385,7 @@ function gcc(chars) { // grammar 'character class' parser builder
 derives(gcc, gts);
 gcc.prototype.emit = function(c) {
   c.r.push(
-    val('cp=i[o]'), // grab the unicode codepoint at the current offset
+    val('cp=+i[o]'), // grab the unicode codepoint at the current offset
     cond(this.con,[[
       val('++o')
     ],[
@@ -431,7 +433,7 @@ function grange(lo,hi) { // grammar range parser builder
 derives(grange, gts);
 grange.prototype.emit = function(c) {
   c.r.push(
-    val('cp=i[o]'),
+    val('cp=+i[o]'),
     cond(lor(ge(o,val("l")),lor(gt(val(this.lo[0]),val("cp")),lt(val(this.hi[0]),val("cp")))),[[
       gotol(this.fail)
     ],[
@@ -443,6 +445,45 @@ grange.prototype.toString = function() {
   return 'range('+this.lo.str.toQuotedString()+','+this.hi.str.toQuotedString()+')';
 };
 grange.prototype.root = range;
+
+function ranges(rangestr) { return new granges(rangestr) }
+function granges(rangestr) { // grammar ranges parser builder
+  gts.call(this); // call the parent constructor
+  this.b = false;
+  this.rangestr = rangestr;
+  // TODO: memoize the generation of this...
+  var nextrange = /([a-fA-F0-9]{4})(-([a-fA-F0-9]{4}))?/g;
+  var cond = '(cp';
+  var mya, lower, upper;
+  while ((mya = nextrange.exec(rangestr)) != null) {
+    lower = parseInt(mya[1],16);
+    if (typeof mya[3] != 'undefined') {
+      upper = parseInt(mya[3],16);
+      cond += '>'+(lower-1)+'&&cp<'+(upper+1)+'||cp';
+    } else {
+      cond += '=='+lower+'||cp';
+    }
+  }
+  this.con = val(''+cond.slice(0,cond.length-4)+')');
+}
+derives(granges, gts);
+granges.prototype.emit = function(c) {
+  c.r.push(
+    val('cp=+i[o]'), // grab the unicode codepoint at the current offset
+    cond(this.con,[[
+      val('++o')
+    ],[
+      gotol(this.fail)
+    ]])
+  );
+};
+granges.prototype.toString = function() {
+  return 'ranges("'+this.rangestr+'")';
+};
+granges.prototype.root = ranges;
+granges.prototype.regen = function() {
+  return new granges(this.rangestr);
+}
 
 function doublec() { return new gdoublec() }
 function gdoublec() { // grammar 'double colon (fail to last alternation)' builder
@@ -1173,7 +1214,8 @@ geither.prototype.emit = function(c) {
   );
   this.l.emit(c);
   c.r.push( // left succeeded
-    gotol(this.notd), // return "not done"
+    (this.xor ? a : val('')),
+    gotol(this.xor ? this.done : this.notd), // return "not done"
     
     casel(this.r.fail), // right failed
     a,
@@ -1185,6 +1227,7 @@ geither.prototype.emit = function(c) {
   );
   this.r.emit(c);
   c.r.push(
+    (this.xor ? a : val('')),
     gotol(this.done)
   );
 };
@@ -1657,9 +1700,9 @@ function utf32str(str) {
 }
 
 function Grammar(name, parent) {
-  this.pats = parent
-    ? derive(parent.pats)
-    : {}; // namespace for patterns
+  this.pats = derive(parent
+    ? parent.pats
+    : Grammar.defaultPatterns); // namespace for patterns
   this.isRoot = !parent; // boolify its definedness
   this.parent = parent;
   this.name = name;
@@ -1855,6 +1898,23 @@ function alt() {
         : arguments[1]
       );
 }
+function oneof() { // exclusive-or. only tested for deterministic patterns
+  return arguments.length == 1
+    ? arguments[0]
+    : xor(arguments[0], 
+      arguments.length > 2
+        ? oneof.apply(null, Array.prototype.slice.call(arguments, 1))
+        : arguments[1]
+      );
+}
+function xor(l,r) {
+  var res = new geither(l,r);
+  res.xor = true;
+  res.regen = function() { return xor(this.l,this.r) };
+  res.toString = function() { return 'xor('+this.l+','+this.r+')' };
+  res.b = false; // mark myself as deterministic, since I am.
+  return res;
+}
 function lits() {
   var args = Array(arguments.length);
   for(var i=0;i<arguments.length;++i)
@@ -1865,6 +1925,24 @@ function lits() {
 function notchars() {
   return seq(neglook(lits.apply(null, arguments)),dot());
 }
+
+Grammar.defaultPatterns = {
+  'alpha':
+  new gcallt(ranges("0041-005A0061-007A00AA00B500BA00C0-00D600D8-00F600F8-02C102C6-02D102E0-02E402EC02EE0370-037403760377037A-037D03860388-038A038C038E-03A103A3-03F503F7-0481048A-05230531-055605590561-058705D0-05EA05F0-05F20621-064A066E066F0671-06D306D506E506E606EE06EF06FA-06FC06FF07100712-072F074D-07A507B107CA-07EA07F407F507FA0904-0939093D09500958-096109710972097B-097F0985-098C098F09900993-09A809AA-09B009B209B6-09B909BD09CE09DC09DD09DF-09E109F009F10A05-0A0A0A0F0A100A13-0A280A2A-0A300A320A330A350A360A380A390A59-0A5C0A5E0A72-0A740A85-0A8D0A8F-0A910A93-0AA80AAA-0AB00AB20AB30AB5-0AB90ABD0AD00AE00AE10B05-0B0C0B0F0B100B13-0B280B2A-0B300B320B330B35-0B390B3D0B5C0B5D0B5F-0B610B710B830B85-0B8A0B8E-0B900B92-0B950B990B9A0B9C0B9E0B9F0BA30BA40BA8-0BAA0BAE-0BB90BD00C05-0C0C0C0E-0C100C12-0C280C2A-0C330C35-0C390C3D0C580C590C600C610C85-0C8C0C8E-0C900C92-0CA80CAA-0CB30CB5-0CB90CBD0CDE0CE00CE10D05-0D0C0D0E-0D100D12-0D280D2A-0D390D3D0D600D610D7A-0D7F0D85-0D960D9A-0DB10DB3-0DBB0DBD0DC0-0DC60E01-0E300E320E330E40-0E460E810E820E840E870E880E8A0E8D0E94-0E970E99-0E9F0EA1-0EA30EA50EA70EAA0EAB0EAD-0EB00EB20EB30EBD0EC0-0EC40EC60EDC0EDD0F000F40-0F470F49-0F6C0F88-0F8B1000-102A103F1050-1055105A-105D106110651066106E-10701075-1081108E10A0-10C510D0-10FA10FC1100-1159115F-11A211A8-11F91200-1248124A-124D1250-12561258125A-125D1260-1288128A-128D1290-12B012B2-12B512B8-12BE12C012C2-12C512C8-12D612D8-13101312-13151318-135A1380-138F13A0-13F41401-166C166F-16761681-169A16A0-16EA1700-170C170E-17111720-17311740-17511760-176C176E-17701780-17B317D717DC1820-18771880-18A818AA1900-191C1950-196D1970-19741980-19A919C1-19C71A00-1A161B05-1B331B45-1B4B1B83-1BA01BAE1BAF1C00-1C231C4D-1C4F1C5A-1C7D1D00-1DBF1E00-1F151F18-1F1D1F20-1F451F48-1F4D1F50-1F571F591F5B1F5D1F5F-1F7D1F80-1FB41FB6-1FBC1FBE1FC2-1FC41FC6-1FCC1FD0-1FD31FD6-1FDB1FE0-1FEC1FF2-1FF41FF6-1FFC2071207F2090-209421022107210A-211321152119-211D212421262128212A-212D212F-2139213C-213F2145-2149214E218321842C00-2C2E2C30-2C5E2C60-2C6F2C71-2C7D2C80-2CE42D00-2D252D30-2D652D6F2D80-2D962DA0-2DA62DA8-2DAE2DB0-2DB62DB8-2DBE2DC0-2DC62DC8-2DCE2DD0-2DD62DD8-2DDE2E2F300530063031-3035303B303C3041-3096309D-309F30A1-30FA30FC-30FF3105-312D3131-318E31A0-31B731F0-31FF34004DB54E009FC3A000-A48CA500-A60CA610-A61FA62AA62BA640-A65FA662-A66EA67F-A697A717-A71FA722-A788A78BA78CA7FB-A801A803-A805A807-A80AA80C-A822A840-A873A882-A8B3A90A-A925A930-A946AA00-AA28AA40-AA42AA44-AA4BAC00D7A3F900-FA2DFA30-FA6AFA70-FAD9FB00-FB06FB13-FB17FB1DFB1F-FB28FB2A-FB36FB38-FB3CFB3EFB40FB41FB43FB44FB46-FBB1FBD3-FD3DFD50-FD8FFD92-FDC7FDF0-FDFBFE70-FE74FE76-FEFCFF21-FF3AFF41-FF5AFF66-FFBEFFC2-FFC7FFCA-FFCFFFD2-FFD7FFDA-FFDC")),
+  'ident': // <.alpha> \w*
+  seq(pref('alpha'),star(pref('\\w'))),
+  '\\w':
+  oneof(pref('alpha'),range('0','9'),cc('_')),
+  'upper':
+  range('A','Z'),
+  'lower':
+  range('a','z'),
+  'digit':
+  range('0','9'),
+  'xdigit':
+  oneof(range('0','9'),range('a','f'),range('A','F')),
+};
+
 
 var dbg = 0;
 /*
@@ -1912,10 +1990,10 @@ g.addPattern('quantified_atom', seq(pref('atom'),ows(),
 
 g.addPattern('alpha', alt(range('a','z'),range('A','Z')));
 
-g.addPattern('backw', alt(pref('alpha'),range('0','9'),cc('_')));
+g.addPattern('back$w', alt(pref('alpha'),range('0','9'),cc('_')));
 
 g.addPattern('atom', alt(
-  seq(pref('backw'),opt(seq(plus(pref('backw')),poslook(pref('backw'))))),
+  seq(pref('back$w'),opt(seq(plus(pref('back$w')),poslook(pref('back$w'))))),
   pref('metachar')));
 
 g.addPattern('backmod', seq(opt(cc(':')),alt(cc('?','!'),neglook(cc(':')))));
@@ -1949,7 +2027,7 @@ g.addToProto('assertion', '!', seq(psym(), alt(poslook(cc('>')), pref('assertion
 
 g.addToProto('assertion', 'method', seq(cc('.'), pref('assertion')));
 
-g.addToProto('assertion', 'name', seq(plus(pref('backw')), opt(alt(
+g.addToProto('assertion', 'name', seq(plus(pref('back$w')), opt(alt(
   poslook(cc('>')),
   seq(cc('='), pref('assertion'))//,
   //seq(lit(':'), pref('arglist')),
@@ -1971,7 +2049,7 @@ g.addPattern('cclass_elem', seq(
     )),
     ows(),
     cc(']')
-  ), plus(pref('backw'))),
+  ), plus(pref('back$w'))),
   ows()
 ));
 
@@ -1990,7 +2068,6 @@ g.compile(); g.parse(utf32str('aa'));
 var g = new Grammar('committest');
 g.addPattern('TOP', seq(star(cc('a')),commit(),cc('a')));
 g.compile(); g.parse(utf32str('aa'));
-*/
 
 var g = new Grammar('priortest');
 g.addPattern('TOP', star(cc('a')));
@@ -1999,6 +2076,12 @@ var g = new Grammar('priortest');
 g.addPattern('TOP', repeat(prior(),2,2));
 g.compile(); g.parse(utf32str('aaaa'));
 
+*/
+
+dbg = 0;
+var g = new Grammar('priortest');
+g.addPattern('TOP', both(star(pref('alpha')),end()));
+g.compile(); g.parse(utf32str('aaD'));
 
 /*
 var sw = new Date();
