78f42784e33898755a13efe5616b3988faa74de9
sorear <sorear@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-06-18 09:55:15 +0000

    [Cursor] Following a so-simple-I-feel-stupid suggestion from pmurias++, we now keep our own copy of part of the Unicode property database.  As a fringe benefit, it's much more similar to the internal form and can be loaded in 0.1s less time.
    git-svn-id: http://svn.pugscode.org/pugs@31356 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ gen-unicode-table.pl
@@ -0,0 +1,36 @@
+#! /usr/local/bin/perl
+use 5.012;  # unicore format is unstable
+use strict;
+use warnings;
+use autodie;
+
+my @tables = (qw|Gc/N Gc/L Perl/Blank Space/Y Perl/VertSpac|);
+
+open DUMP, ">", "uniprops";
+binmode DUMP;
+
+for my $propname (@tables) {
+    my @top = (("\0" x 128) x 1088);
+    my $used = "\0" x 136;
+    for my $l (split("\n", (do "unicore/lib/$propname.pl"))) {
+        my ($from, $to) = split("\t", $l);
+        $from = hex $from;
+        $to = hex $to || $from;
+
+        for (my $x = $from; $x <= $to; $x++) {
+            vec($top[$x >> 10], $x & 1023, 1) = 1;
+            vec($used, $x >> 10, 1) = 1;
+        }
+    }
+
+    print DUMP chr(length($propname));
+    print DUMP $propname;
+    print DUMP $used;
+    for (my $i = 0; $i < 1088; $i++) {
+        if (vec($used, $i, 1)) {
+            print DUMP $top[$i];
+        }
+    }
+}
+
+close DUMP;
