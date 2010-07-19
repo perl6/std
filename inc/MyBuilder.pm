495c84f986b79bb47b44bd05b345587f2fb0ebf2
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-07-19 10:10:04 +0000

    [mildew][smop][STD] bump version fix mangle.pl installating (found and fixed by au++) update mildew to use {YOU_ARE_HERE}
    git-svn-id: http://svn.pugscode.org/pugs@31764 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ inc/MyBuilder.pm
@@ -0,0 +1,14 @@
+package MyBuilder;
+use base 'Module::Build';
+
+use warnings;
+use strict;
+
+
+use v5.10;
+
+sub new {
+    my ($self,@args) = @_;
+    $self->SUPER::new('pm_files'=>{map {$_ => $_} <lib/*>},@args);
+}
+1;
